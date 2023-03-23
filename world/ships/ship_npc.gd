4b2df81c5749a990a532aa9f04065af993f7f469
Martin Riesz <riesz.martin@gmail.com>
2023-03-23 15:04:32 +0100

    Comment out code

--- world/ships/ship_npc.gd
+++ world/ships/ship_npc.gd
@@ -13,13 +13,13 @@ func _physics_process(delta):
 	pass
 
 
-func look_follow(state, current_transform, target_position):
-	var cur_dir = current_transform.basis * Vector3.BACK
-	var target_dir = (target_position - current_transform.origin).normalized()
-	var rotation_angle = acos(cur_dir.x) - acos(target_dir.x)
-	state.angular_velocity = Vector3.UP * (rotation_angle / state.step)
-
-func _integrate_forces(state):
-	var target: Node3D = $"../Bob"
-	var target_position = target.global_transform.origin
-	look_follow(state, global_transform, target_position)
+#func look_follow(state: PhysicsDirectBodyState3D, target_position):
+#	var target_dir = (target_position - global_transform.origin).normalized()
+#	var cur_dir = global_transform.basis * Vector3.FORWARD
+#	var rotation_angle = acos(cur_dir.x) - acos(target_dir.x)
+#	state.angular_velocity = Vector3.UP * (rotation_angle / state.step)
+#
+#func _integrate_forces(state):
+#	var target: Node3D = $"../Bob"
+#	print(target.global_transform.origin)
+#	look_follow(state, target.global_transform.origin)
