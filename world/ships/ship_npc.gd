516a8063105f6fc63704661102a7d52f6c3bfeca
Martin Riesz <riesz.martin@gmail.com>
2023-03-24 19:56:17 +0100

    Implement working looking at with RigidBody3D

--- world/ships/ship_npc.gd
+++ world/ships/ship_npc.gd
@@ -1,5 +1,4 @@
 extends RigidBody3D
-#extends CharacterBody3D
 
 
 func _ready():
@@ -8,19 +7,21 @@ func _ready():
 		randf_range(-100, 100),
 		randf_range(-100, 100),
 	)
+	can_sleep = false
+	gravity_scale = 0
+	linear_damp = 5
+	angular_damp = 5
 
 
 func _physics_process(_delta):
 	pass
 
 
-#func look_follow(state: PhysicsDirectBodyState3D, target_position):
-#	var target_dir = (target_position - global_transform.origin).normalized()
-#	var cur_dir = global_transform.basis * Vector3.FORWARD
-#	var rotation_angle = acos(cur_dir.x) - acos(target_dir.x)
-#	state.angular_velocity = Vector3.UP * (rotation_angle / state.step)
-#
-#func _integrate_forces(state):
-#	var target: Node3D = $"../Bob"
-#	print(target.global_transform.origin)
-#	look_follow(state, target.global_transform.origin)
+func look_follow(state: PhysicsDirectBodyState3D, target_position: Vector3):
+	var target_dir := (target_position - global_position).normalized()
+	var current_dir := global_transform.basis.z
+	var correction := target_dir - current_dir
+	state.angular_velocity = correction.cross(-global_transform.basis.z).normalized() * correction.length()
+
+func _integrate_forces(state):
+	look_follow(state, $"../Bob".global_position)
