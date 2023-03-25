16d59e418c2cd91a62f3e8b1ed89768ac7f9db32
Martin Riesz <riesz.martin@gmail.com>
2023-03-25 14:20:55 +0100

    Add export to Android, improve FPS monitor on Android

--- singletons/fps_monitor.gd
+++ singletons/fps_monitor.gd
@@ -5,6 +5,10 @@ extends Node
 func _ready():
 	label.set_anchors_preset(Control.PRESET_FULL_RECT)
 	label.set_horizontal_alignment(HORIZONTAL_ALIGNMENT_RIGHT)
+	if OS.get_name() == "Android":
+		label.label_settings = LabelSettings.new()
+		label.label_settings.font_size = 30
+		label.position.x = -50  # some phones have rounded corners
 	add_child(label)
 
 
