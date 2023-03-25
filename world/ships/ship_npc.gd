dcf16f1beabe182e355928570c30db1e832eae95
Martin Riesz <riesz.martin@gmail.com>
2023-03-25 12:33:53 +0100

    Simplify npc movement

--- world/ships/ship_npc.gd
+++ world/ships/ship_npc.gd
@@ -15,30 +15,28 @@ func _ready():
 	angular_damp = 5
 
 
-func point_at(target_position: Vector3, only_if_farther_than = 10):
-	if (target_position - global_position).length() < only_if_farther_than:
+func point_at(target_position: Vector3, min_distance = 10):
+	if global_position.distance_to(target_position) < min_distance:
 		return true
-	var target_dir := (target_position - global_position).normalized()
+	var target_dir := global_position.direction_to(target_position)
 	var current_dir := global_transform.basis.z
 	var correction := target_dir - current_dir
 	self.apply_torque(correction.cross(-global_transform.basis.z).normalized() * correction.length() * 100)
 	return correction.length() < 0.1
 
 
-func match_roll_with(target: Node3D, ease_out = false):
-	if ease_out:
-		var correction := Plane(global_transform.basis.z).project(target.global_transform.basis.y) - global_transform.basis.y
-		self.apply_torque(correction.cross(-global_transform.basis.y).normalized() * correction.length() * 100)
-		return correction.length() < 0.1
-	else:
-		var correction := target.global_transform.basis.y.signed_angle_to(global_transform.basis.y, -global_transform.basis.z)
-		self.apply_torque(global_transform.basis.z * correction * 50)
-		return correction < 0.1
+func match_roll_with(target: Node3D):
+	var projected_target_up := Plane(global_transform.basis.z).project(target.global_transform.basis.y)
+	if projected_target_up.length() < 0.1:
+		return true  # prevent constant rolling due to parallel forward and target up vectors
+	var correction := projected_target_up - global_transform.basis.y
+	self.apply_torque(correction.cross(-global_transform.basis.y).normalized() * correction.length() * 100)
+	return correction.length() < 0.2
 
 
 func move_to_position(target_position: Vector3):
 	var correction := target_position - global_position
-	self.apply_central_force(correction.normalized() * correction.length() * 4)
+	self.apply_central_force(correction * 4)
 
 
 func move_to(target: Node3D):
@@ -46,6 +44,6 @@ func move_to(target: Node3D):
 
 
 func _physics_process(_delta):
-	if point_at(player.global_transform * Vector3(0, 0, 20)):
-		match_roll_with(player, true)
-		move_to_position(player.global_transform * Vector3(0, 0, 20))
+	point_at(player.global_position)
+	match_roll_with(player)
+	move_to_position(player.global_transform * Vector3(0, 0, 20))
