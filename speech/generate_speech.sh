4813268d5777d9cc07fdcf9ab092ca3c462b747c
Martin Riesz <riesz.martin@gmail.com>
2023-03-29 18:20:44 +0200

    Create virtualenv with tts automatically

--- speech/generate_speech.sh
+++ speech/generate_speech.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+set -e
 script_dir=$(dirname "$(realpath $0)")
+virtualenv_dir="$script_dir/.venv"
+
+if [ ! -d "$virtualenv_dir" ]; then
+    python -m venv "$virtualenv_dir"
+fi
+source "$virtualenv_dir/bin/activate"
+if [ ! -f "$virtualenv_dir/bin/tts" ]; then
+    $virtualenv_dir/bin/pip install tts
+fi
 project_root=$(dirname "$script_dir")
 output_dir="$script_dir/generated"
 temp_dir=$(mktemp -d -q)
