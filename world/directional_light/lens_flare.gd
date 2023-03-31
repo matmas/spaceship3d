93d12eab13b834160d1faba2ad27fd6fc4d12b6d
Martin Riesz <riesz.martin@gmail.com>
2023-03-31 03:35:02 +0200

    Use raycast to hide lens flare

--- world/directional_light/lens_flare.gd
+++ world/directional_light/lens_flare.gd
@@ -1,16 +1,25 @@
 extends TextureRect
 
+@onready var camera: Camera3D = get_viewport().get_camera_3d()
 @onready var directional_light: DirectionalLight3D = get_parent()
 
 
+func get_light_apparent_global_position() -> Vector3:
+	return camera.global_position + directional_light.global_transform.basis.z * camera.far
+
+
 func _process(_delta):
-	var camera := get_viewport().get_camera_3d()
-	var effective_sun_direction: Vector3 = directional_light.global_transform.basis.z * maxf(camera.near, 1.0)
-	effective_sun_direction += camera.global_transform.origin
+#	visible = not camera.is_position_behind(effective_sun_direction)
+#	if not camera.is_position_behind(effective_sun_direction):
+	var unprojected_sun_position: Vector2 = camera.unproject_position(get_light_apparent_global_position())
+	var shader := material as ShaderMaterial
+	shader.set_shader_parameter("sun_position", unprojected_sun_position)
 
-	visible = not camera.is_position_behind(effective_sun_direction)
 
-	if visible:
-		var unprojected_sun_position: Vector2 = camera.unproject_position(effective_sun_direction)
-		var shader := material as ShaderMaterial
-		shader.set_shader_parameter("sun_position", unprojected_sun_position)
+func _physics_process(delta):
+	var space_state = directional_light.get_world_3d().direct_space_state
+	var params := PhysicsRayQueryParameters3D.new()
+	params.from = camera.global_position
+	params.to = get_light_apparent_global_position()
+	var result := space_state.intersect_ray(params)
+	visible = false if result else true
