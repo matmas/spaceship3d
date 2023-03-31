04ac34424d08c9ff16cbfe8c054526c7c42074e8
Martin Riesz <riesz.martin@gmail.com>
2023-03-31 23:00:39 +0200

    Add time-based dimming for lens flare

--- world/directional_light/lens_flare.gd
+++ world/directional_light/lens_flare.gd
@@ -4,13 +4,20 @@ extends TextureRect
 @onready var directional_light: DirectionalLight3D = get_parent()
 
 
+var visibility := 0.0
+var target_visibility := 0.0
+
+
 func get_light_apparent_global_position() -> Vector3:
 	return camera.global_position + directional_light.global_transform.basis.z * camera.far
 
 
-func _process(_delta):
+func _process(delta):
 	var shader := material as ShaderMaterial
 	shader.set_shader_parameter("sun_position", camera.unproject_position(get_light_apparent_global_position()))
+	shader.set_shader_parameter("visibility", visibility)
+	visibility = lerp(visibility, target_visibility, delta * 10)
+	visible = not camera.is_position_behind(get_light_apparent_global_position())
 
 
 func _physics_process(_delta):
@@ -19,4 +26,4 @@ func _physics_process(_delta):
 	params.from = camera.global_position
 	params.to = get_light_apparent_global_position()
 	var result := space_state.intersect_ray(params)
-	visible = false if result else not camera.is_position_behind(get_light_apparent_global_position())
+	target_visibility = 0.0 if result else 1.0
