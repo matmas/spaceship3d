7fc0ea01cd07d09b83a2153385a8ca123a51c632
Martin Riesz <riesz.martin@gmail.com>
2023-03-31 22:37:11 +0200

    Revert "Convert LensFlare to spatial shader"
    This reverts commit 105d888ef55864de60d2c22849feb634d93ba30f.

--- world/directional_light/lens_flare.gd
+++ world/directional_light/lens_flare.gd
@@ -1,4 +1,4 @@
-extends MeshInstance3D
+extends TextureRect
 
 @onready var camera: Camera3D = get_viewport().get_camera_3d()
 @onready var directional_light: DirectionalLight3D = get_parent()
@@ -9,6 +9,14 @@ func get_light_apparent_global_position() -> Vector3:
 
 
 func _process(_delta):
-	var shader := get_active_material(0) as ShaderMaterial
+	var shader := material as ShaderMaterial
 	shader.set_shader_parameter("sun_position", camera.unproject_position(get_light_apparent_global_position()))
-	visible = not camera.is_position_behind(get_light_apparent_global_position())
+
+
+func _physics_process(_delta):
+	var space_state = directional_light.get_world_3d().direct_space_state
+	var params := PhysicsRayQueryParameters3D.new()
+	params.from = camera.global_position
+	params.to = get_light_apparent_global_position()
+	var result := space_state.intersect_ray(params)
+	visible = false if result else not camera.is_position_behind(get_light_apparent_global_position())
