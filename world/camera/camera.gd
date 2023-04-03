67d329d4ca0bd404a0f1ce4000ebc90ebf003098
Martin Riesz <riesz.martin@gmail.com>
2023-04-03 17:56:36 +0200

    Readd raycasting and fix interpolation bug

--- world/camera/camera.gd
+++ world/camera/camera.gd
@@ -1,12 +1,12 @@
 extends Camera3D
 
-@onready var target: Node3D = Globals.player.get_node("Bob")
-#@onready var collision_exclude := Globals.player
+@onready var target_mesh := Globals.player.get_node("Bob") as MeshInstance3D
+@onready var target_body := Globals.player as PhysicsBody3D
 
 var camera_distance := 10.0
 var camera_relative_direction := Vector3(0.0, 2.0, -10.0).normalized()
 var current_target_camera_position: Vector3
-#var previous_target_camera_position: Vector3
+var previous_target_camera_position: Vector3
 
 
 func _ready():
@@ -19,29 +19,29 @@ func _process(delta: float):
 
 func _target_camera_transform() -> Transform3D:
 	var f := Engine.get_physics_interpolation_fraction()
-	var target_camera_position := target.global_transform * (camera_relative_direction * camera_distance)
-#	var target_camera_position := previous_target_camera_position.lerp(current_target_camera_position, f)
-	var target_camera_basis = target.global_transform.rotated_local(Vector3.UP, TAU / 2).basis
+#	var target_camera_position := target_mesh.global_transform * (camera_relative_direction * camera_distance)
+	var target_camera_position := previous_target_camera_position.lerp(current_target_camera_position, f)
+	var target_camera_basis = target_mesh.global_transform.rotated_local(Vector3.UP, TAU / 2).basis
 	return Transform3D(target_camera_basis, target_camera_position)
 
 
-#func _physics_process(_delta):
-#	previous_target_camera_position = current_target_camera_position
-#	current_target_camera_position = _calculate_target_camera_position()
+func _physics_process(_delta):
+	previous_target_camera_position = current_target_camera_position
+	current_target_camera_position = _calculate_target_camera_position()
 
 
-#func _calculate_target_camera_position() -> Vector3:
-#	var farthest_point := target.global_transform * (camera_relative_direction * camera_distance)
-#	var space_state = get_world_3d().direct_space_state
-#	var params := PhysicsRayQueryParameters3D.new()
-#	params.from = target.global_position
-#	params.to = farthest_point
-#	params.exclude = [collision_exclude]
-#	var result := space_state.intersect_ray(params)
-#	if result:
-#		return result["position"]
-#	else:
-#		return farthest_point
+func _calculate_target_camera_position() -> Vector3:
+	var farthest_point := target_body.global_transform * (camera_relative_direction * camera_distance)
+	var space_state = get_world_3d().direct_space_state
+	var params := PhysicsRayQueryParameters3D.new()
+	params.from = target_body.global_position
+	params.to = farthest_point
+	params.exclude = [target_body]
+	var result := space_state.intersect_ray(params)
+	if result:
+		return result["position"]
+	else:
+		return farthest_point
 
 
 func _input(event):
