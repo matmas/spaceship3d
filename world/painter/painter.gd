0055b8fecdaa2181854af4fed4df9857fdd1bd27
Martin Riesz <riesz.martin@gmail.com>
2023-04-09 02:11:36 +0200

    Setup material only once

--- world/painter/painter.gd
+++ world/painter/painter.gd
@@ -5,6 +5,7 @@ extends Node
 @onready var canvas_scene := preload("res://world/painter/canvas.tscn")
 
 var _canvases := {}
+var _materials := {}
 
 
 func _ready():
@@ -20,23 +21,29 @@ func _paint(mesh_instance: MeshInstance3D, from_position: Vector3, to_position: 
 	var color := image.get_pixel(image.get_width() / 2, image.get_height() / 2)  # warning-ignore:integer_division
 	var uv := Vector2(color.r, color.g)
 
-	var canvas := get_canvas(mesh_instance)
+	var canvas := _get_or_create_canvas(mesh_instance)
 	var brush := canvas.get_node("Brush")
 
 	brush.position = uv * canvas.get_texture().get_size()
 	canvas.render_target_update_mode = SubViewport.UPDATE_ONCE
-	var material := mesh_instance.get_active_material(0)
-	if material is BaseMaterial3D:
-		var base_material := material as BaseMaterial3D
-		var copy := base_material.duplicate()
-		mesh_instance.set_surface_override_material(0, copy)
-		copy.detail_enabled = true
-		copy.detail_mask = canvas.get_texture()
+	_setup_material(mesh_instance, canvas)
 
 
-func get_canvas(mesh_instance: MeshInstance3D) -> SubViewport:
+func _get_or_create_canvas(mesh_instance: MeshInstance3D) -> SubViewport:
 	if mesh_instance not in _canvases:
 		var canvas := canvas_scene.instantiate()
 		add_child(canvas)
 		_canvases[mesh_instance] = canvas
 	return _canvases[mesh_instance] as SubViewport
+
+
+func _setup_material(mesh_instance: MeshInstance3D, canvas: SubViewport):
+	var material := mesh_instance.get_active_material(0)
+	if material is BaseMaterial3D:
+		var base_material := material as BaseMaterial3D
+		if mesh_instance not in _materials:
+			var copy := base_material.duplicate()
+			mesh_instance.set_surface_override_material(0, copy)
+			copy.detail_enabled = true
+			copy.detail_mask = canvas.get_texture()
+			_materials[mesh_instance] = copy
