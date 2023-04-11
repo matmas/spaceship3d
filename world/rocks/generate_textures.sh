1c756ef0ddf94be8f73469a0ccbae036acf7a75d
Martin Riesz <riesz.martin@gmail.com>
2023-04-11 20:09:26 +0200

    Move rock textures to world/rocks

--- world/rocks/generate_textures.sh
+++ world/rocks/generate_textures.sh
@@ -2,6 +2,6 @@
 engine="Godot/G4 ORM"
 scriptpath=$(realpath "$0")
 scriptdir=$(dirname "$scriptpath")
-output_path="$scriptdir/../generated"
+output_path="$scriptdir"
 input_files=*.ptex
 material-maker --export-material --target "$engine" -o "$output_path" "$input_files"
