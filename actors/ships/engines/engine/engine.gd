d16466a877152b655e732b3d20d4949bc0659dd5
Martin Riesz <riesz.martin@gmail.com>
2023-04-25 21:31:00 +0200

    Import and use jet cone mesh as OBJ
    .blend (primary, scene) binary
    .obj (primary, object) text
    .gltf (triangulated) text
    .glb (compressed .gltf) binary
    .tres (godot custom parameters, object) text
    .res (binary .tres) binary
    .fbx (proprietary) binary
    
    Use .obj for objects as a primary format if we only need vertices, normals, one UV map, vertex colors and simple materials
    Use .gltf for scenes if we don't need quads
    Use .tres for objects if we don't need quads but need godot specific parameters
    Use .blend for scenes as a primary format for everything else
    Convert .res to .tres as git will use zlib compression anyway
    Convert .fbx to .gltf
    Convert .glb to .gltf or keep .glb if we don't expect modifications

--- actors/ships/engines/engine/engine.gd
+++ actors/ships/engines/engine/engine.gd
@@ -1,8 +1,8 @@
 class_name ShipEngine extends Node3D
 
-@onready var exhaust := $Exhaust as AudioStreamPlayer3D
-@onready var cone = $"Cone/Cone" as MeshInstance3D
+@onready var flame := $Flame as MeshInstance3D
 @onready var distortion := $Distortion as MeshInstance3D
+@onready var exhaust := $Exhaust as AudioStreamPlayer3D
 
 var power := 0.0
 var target_power := 0.0
@@ -10,7 +10,7 @@ var target_power := 0.0
 
 func _ready() -> void:
 	# We need to change each engine material independently
-	cone.set_surface_override_material(0, cone.get_active_material(0).duplicate() as Material)
+	flame.set_surface_override_material(0, flame.get_active_material(0).duplicate() as Material)
 	distortion.set_surface_override_material(0, distortion.get_active_material(0).duplicate() as Material)
 
 
@@ -20,10 +20,10 @@ func set_power(value: float) -> void:
 
 func _process(delta: float) -> void:
 	power = lerpf(power, target_power, 1 - pow(0.1, delta * 5))
-	var material := cone.get_active_material(0) as ShaderMaterial
-	material.set_shader_parameter(&"alpha_multiplier", power)
-	material.set_shader_parameter(&"gradient_multiplier", power * 6)
-	material.set_shader_parameter(&"gradient_shift", power)
+	var flame_material := flame.get_active_material(0) as ShaderMaterial
+	flame_material.set_shader_parameter(&"alpha_multiplier", power)
+	flame_material.set_shader_parameter(&"gradient_multiplier", power * 6)
+	flame_material.set_shader_parameter(&"gradient_shift", power)
 	exhaust.volume_db = linear_to_db(power * 0.1)
 
 	var distortion_material := distortion.get_active_material(0) as ShaderMaterial
