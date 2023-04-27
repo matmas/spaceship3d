e0626b9c3dcad19751aac6ddcdcc7031a8941cc2
Martin Riesz <riesz.martin@gmail.com>
2023-04-27 14:46:22 +0200

    Fix light moving with particles not emitting

--- actors/weapons/laser/sparks/light.gd
+++ actors/weapons/laser/sparks/light.gd
@@ -3,17 +3,18 @@ extends OmniLight3D
 @onready var default_energy := light_energy
 @onready var particles := get_parent() as GPUParticles3D
 
-var intensity = 0.0
+var intensity := 0.0
 var noise := FastNoiseLite.new()
 
 
 func _ready() -> void:
-	# light should not move relative to the parent when local_coords if false (default)
+	# light should not move relative to the parent unless local_coords if true
 	top_level = not particles.local_coords
 
 
 func _process(delta: float) -> void:
-	global_position = particles.global_position
+	if particles.emitting:
+		global_position = particles.global_position
 	var target_intensity := float(particles.emitting)
 	intensity = move_toward(intensity, target_intensity, delta / particles.lifetime)
 	light_energy = intensity * default_energy * ((noise.get_noise_1d(Time.get_ticks_msec()) + 1) * 0.25 + 0.5)
