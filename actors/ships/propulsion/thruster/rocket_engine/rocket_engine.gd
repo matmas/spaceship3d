aba2a37ef2bcd44e3815c33fc0a9154f4b03d12e
Martin Riesz <riesz.martin@gmail.com>
2023-04-29 01:31:22 +0200

    Add rocket engine light

--- actors/ships/propulsion/thruster/rocket_engine/rocket_engine.gd
+++ actors/ships/propulsion/thruster/rocket_engine/rocket_engine.gd
@@ -1,15 +1,21 @@
 extends Thruster
 
 @onready var distortion := $Distortion as MeshInstance3D
+@onready var light := $Light as OmniLight3D
+@onready var light_energy := light.light_energy
+
+var noise := FastNoiseLite.new()
 
 
 func _ready() -> void:
 	super._ready()
 	# We need to change each material independently
 	distortion.set_surface_override_material(0, distortion.get_active_material(0).duplicate() as Material)
+	light.light_energy = 0
 
 
 func _process(delta: float) -> void:
 	super._process(delta)
 	var distortion_material := distortion.get_active_material(0) as ShaderMaterial
 	distortion_material.set_shader_parameter(&"distortion", power * 0.005)
+	light.light_energy = power * light_energy * ((noise.get_noise_1d(Time.get_ticks_msec()) + 1) * 0.25 + 0.5)
