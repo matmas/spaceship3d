226a0a14a436ab719660ad4595aa762543572af6
Martin Riesz <riesz.martin@gmail.com>
2023-04-29 17:35:01 +0200

    Replace laser sound with Horror Ambience 4 from Mrrobodevin at freesound.org (CC0)
    Cut from 52s to 2s loop
    
    https://freesound.org/people/Mrrobodevin/sounds/658165/

--- actors/weapons/laser/laser.gd
+++ actors/weapons/laser/laser.gd
@@ -8,7 +8,6 @@ extends RayCast3D
 @onready var exclude := owner as CollisionObject3D
 @onready var painter := $Painter as Painter
 @onready var firing := $Firing as AudioStreamPlayer3D
-@onready var hitting := $Hitting as AudioStreamPlayer3D
 
 var targetting_speed := 1000.0
 
@@ -32,8 +31,8 @@ func _process(delta: float) -> void:
 	beam.scale.x = power
 	beam.scale.y = power
 
-	firing.volume_db = linear_to_db(power) if not is_colliding() else -INF
-	hitting.volume_db = linear_to_db(power) if is_colliding() else -INF
+	firing.volume_db = linear_to_db(power)
+	firing.pitch_scale = 1.0 if is_colliding() else 0.5
 
 	var ray_origin := camera.project_ray_origin(Mouse.get_cursor_position())
 	var ray_end := ray_origin + camera.project_ray_normal(Mouse.get_cursor_position()) * camera.far
