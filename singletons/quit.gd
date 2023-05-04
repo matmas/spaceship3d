420931e1c2799596cc51a7f2f7df271bc66b55af
Martin Riesz <riesz.martin@gmail.com>
2023-05-04 12:01:03 +0200

    Disable quitting with Ctrl+Q

--- singletons/quit.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if event.pressed and event.get_physical_keycode_with_modifiers() == KEY_MASK_CTRL | KEY_Q:
-			get_tree().quit()
