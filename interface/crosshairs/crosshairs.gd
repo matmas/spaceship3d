950ed8d80792907b856f9bd694219b523f62e33a
Martin Riesz <riesz.martin@gmail.com>
2023-05-10 23:33:45 +0200

    Return back to simple center indicator that is always visible

--- interface/crosshairs/crosshairs.gd
+++ interface/crosshairs/crosshairs.gd
@@ -1,7 +1,8 @@
 extends Node
 
 @onready var steering := $Steering as Sprite2D
-@onready var steering_material := steering.material as ShaderMaterial
+@onready var center_indicator := $CenterIndicator as Sprite2D
+@onready var center_indicator_material := center_indicator.material as ShaderMaterial
 
 
 func _ready() -> void:
@@ -11,5 +12,6 @@ func _ready() -> void:
 
 func _update_crosshairs() -> void:
 	var center := Vector2(get_viewport().size) * 0.5
+
 	steering.position = Mouse.get_cursor_position()
-	steering_material.set_shader_parameter(&"indicator_position", (center - steering.position) / Vector2(get_viewport().size))
+	center_indicator.position = center
