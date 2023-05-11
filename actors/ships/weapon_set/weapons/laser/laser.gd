994d2bbfdb1bb99113e466129cf6f05e506f08e5
Martin Riesz <riesz.martin@gmail.com>
2023-05-12 01:31:23 +0200

    Move gimbal logic from laser to weapon

--- actors/ships/weapon_set/weapons/laser/laser.gd
+++ actors/ships/weapon_set/weapons/laser/laser.gd
@@ -6,34 +6,24 @@ extends Weapon
 @onready var beam_scale := beam.scale
 @onready var sparks := $Sparks as GPUParticles3D
 @onready var smoke := $Smoke as GPUParticles3D
-@onready var camera := get_viewport().get_camera_3d() as Camera3D
-@onready var exclude := owner.owner as CollisionObject3D
 @onready var painter := $Painter as Painter
 @onready var firing := $Firing as AudioStreamPlayer3D
 @onready var firing_volume := firing.volume_db
 @onready var hitting := $Hitting as AudioStreamPlayer3D
 @onready var hitting_volume := hitting.volume_db
 
-var targetting_speed := 5.0
-
 var power := 0.0
-var target_position_override := Vector3()
-
-
-func _enter_tree() -> void:
-	if not owner:
-		owner = get_parent().owner if get_parent().scene_file_path == "" else get_parent()
 
 
 func _ready() -> void:
-	ray_cast.add_exception(exclude)
-	set_process(visible)
-	set_physics_process(visible)
+	super._ready()
+	ray_cast.add_exception(owner.owner)
 	firing.volume_db = -INF
 	hitting.volume_db = -INF
 
 
 func _process(delta: float) -> void:
+	super._process(delta)
 	if is_firing:
 		power = move_toward(power, 1.0, delta * 60 * 0.2)
 	else:
@@ -47,11 +37,6 @@ func _process(delta: float) -> void:
 	firing.volume_db = firing_volume + linear_to_db(lerpf(db_to_linear(firing.volume_db), power, 1 - pow(0.1, delta * 5)))
 	hitting.volume_db = hitting_volume if ray_cast.is_colliding() else -INF
 	hitting.global_position = ray_cast.get_collision_point()
-
-	if not is_fixed:
-		var new_target_position := target_position_override if target_position_override else _mouse_cursor_to_world_position()
-		var new_basis := Basis.looking_at(global_position.direction_to(new_target_position))
-		global_transform.basis = global_transform.basis.slerp(new_basis, 1 - pow(0.1, delta * targetting_speed)).orthonormalized()
 
 	var beam_endpoint := ray_cast.get_collision_point() if ray_cast.is_colliding() else to_global(ray_cast.target_position)
 	for p in [sparks, smoke]:
@@ -69,26 +54,9 @@ func _process(delta: float) -> void:
 		painter.paint_line_on(ray_cast.get_collider() as CollisionObject3D, global_transform)
 
 
-
-func _physics_process(_delta: float) -> void:
-	if not is_fixed:
-		target_position_override = _mouse_cursor_to_collision_world_position()
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
 	if ray_cast.is_colliding() and power == 1.0:
 		if ray_cast.get_collider() is Ship:
 			var ship := ray_cast.get_collider() as Ship
 			ship.hit.emit(self)
-
-
-func _mouse_cursor_to_world_position() -> Vector3:
-	var ray_origin := camera.project_ray_origin(Mouse.get_cursor_position())
-	return ray_origin + camera.project_ray_normal(Mouse.get_cursor_position()) * camera.far
-
-
-func _mouse_cursor_to_collision_world_position() -> Vector3:
-	var params := PhysicsRayQueryParameters3D.new()
-	params.from = camera.project_ray_origin(Mouse.get_cursor_position())
-	params.to = params.from + camera.project_ray_normal(Mouse.get_cursor_position()) * camera.far
-	params.exclude = [exclude]
-	var result := get_world_3d().direct_space_state.intersect_ray(params)
-	# Avoid targetting empty space just in front body by moving collision point a bit forward
-	return to_global(to_local(result.position) + Vector3.FORWARD * 0.1) if result else Vector3()
--- actors/ships/weapon_set/weapons/weapon.gd
+++ actors/ships/weapon_set/weapons/weapon.gd
@@ -1,5 +1,46 @@
 class_name Weapon
 extends Node3D
 
+@onready var camera := get_viewport().get_camera_3d() as Camera3D
+
+var targetting_speed := 1.0
+var target_position_override := Vector3()
 var is_fixed := false
 var is_firing := false
+
+
+func _enter_tree() -> void:
+	if not owner:
+		owner = get_parent().owner if get_parent().scene_file_path == "" else get_parent()
+
+
+func _ready() -> void:
+	set_process(visible)
+	set_physics_process(visible)
+
+
+func _process(delta: float) -> void:
+	if not is_fixed:
+		var new_target_position := target_position_override if target_position_override else _mouse_cursor_to_world_position()
+		var new_basis := Basis.looking_at(global_position.direction_to(new_target_position))
+		global_transform.basis = global_transform.basis.slerp(new_basis, 1 - pow(0.1, delta * targetting_speed)).orthonormalized()
+
+
+func _physics_process(_delta: float) -> void:
+	if not is_fixed:
+		target_position_override = _mouse_cursor_to_collision_world_position()
+
+
+func _mouse_cursor_to_world_position() -> Vector3:
+	var ray_origin := camera.project_ray_origin(Mouse.get_cursor_position())
+	return ray_origin + camera.project_ray_normal(Mouse.get_cursor_position()) * camera.far
+
+
+func _mouse_cursor_to_collision_world_position() -> Vector3:
+	var params := PhysicsRayQueryParameters3D.new()
+	params.from = camera.project_ray_origin(Mouse.get_cursor_position())
+	params.to = params.from + camera.project_ray_normal(Mouse.get_cursor_position()) * camera.far
+	params.exclude = [owner.owner]
+	var result := get_world_3d().direct_space_state.intersect_ray(params)
+	# Avoid targetting empty space just in front body by moving collision point a bit forward
+	return to_global(to_local(result.position) + Vector3.FORWARD * 0.1) if result else Vector3()
