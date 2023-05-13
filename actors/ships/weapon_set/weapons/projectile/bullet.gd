e8233ca1b1a1358414970a9c49a181ea3413efb0
Martin Riesz <riesz.martin@gmail.com>
2023-05-14 01:37:34 +0200

    Use bullet scene with MeshInstance3D and raycasting in physics process
    10k objects total
    raycasting in _process: ~78 FPS, ~13ms process
    raycasting in _physics_process: 100-140FPS, 6ms process, 40ms physics process, jittery graphics

--- actors/ships/weapon_set/weapons/projectile/bullet.gd
+++ actors/ships/weapon_set/weapons/projectile/bullet.gd
@@ -1,25 +1,19 @@
 class_name Bullet
-extends RefCounted
-
-var transform: Transform3D
-var linear_velocity: Vector3
-var exclude: Array[RID]
-var world: World3D
+extends Node3D
 
 
-func _init(bullet_transform: Transform3D, bullet_linear_velocity: Vector3, excluded: Array[RID], world3d: World3D) -> void:
-	transform = bullet_transform
-	linear_velocity = bullet_linear_velocity
-	exclude = excluded
-	world = world3d
+var linear_velocity := Vector3()
 
 
-func move_and_collide(delta: float) -> Vector3:
-	var new_position := transform.origin + linear_velocity * delta
+
+func _physics_process(delta: float) -> void:
+	var new_position := global_transform.origin + linear_velocity * delta
 	var params := PhysicsRayQueryParameters3D.new()
-	params.from = transform.origin
+	params.from = global_transform.origin
 	params.to = new_position
-	params.exclude = exclude
-	var result := world.direct_space_state.intersect_ray(params)
-	transform.origin = new_position
-	return result.position if result else Vector3()
+#	params.exclude = exclude
+	var result := get_world_3d().direct_space_state.intersect_ray(params)
+	global_transform.origin = new_position
+#	return result.position if result else Vector3()
+	if result:
+		queue_free()
--- actors/ships/weapon_set/weapons/projectile/projectile.gd
+++ actors/ships/weapon_set/weapons/projectile/projectile.gd
@@ -1,27 +1,14 @@
 extends Weapon
 
-@onready var bullet_multimesh: MultiMeshInstance3D = $BulletMultimesh
-@onready var exclude := [owner.owner]
+@onready var bullet_scene := preload("bullet.tscn")
 
 var cooldown := 1.0
 var bullet_velocity := 10.0
-var bullets := {}
 
 
-func _ready() -> void:
-	bullet_multimesh.top_level = true
-
-
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_firing:
-		bullets[Bullet.new(global_transform, -global_transform.basis.z * bullet_velocity, [], get_world_3d())] = true
-
-	for bullet in bullets:
-		if bullet.move_and_collide(delta):
-			bullets.erase(bullet)
-
-	bullet_multimesh.multimesh.instance_count = bullets.size()
-	var i := 0
-	for bullet in bullets:
-		bullet_multimesh.multimesh.set_instance_transform(i, bullet.transform)
-		i += 1
+		var bullet := bullet_scene.instantiate() as Bullet
+		bullet.top_level = true
+		bullet.linear_velocity = -global_transform.basis.z * bullet_velocity
+		add_child(bullet)
