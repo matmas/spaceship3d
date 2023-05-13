d6fc135654d696cddf2f0ecd25a33e89479dcc91
Martin Riesz <riesz.martin@gmail.com>
2023-05-14 01:52:37 +0200

    Use RayCast3D node for bullet
    10k objects
    ~130 FPS
    6ms process
    30ms physics process

--- actors/ships/weapon_set/weapons/projectile/bullet.gd
+++ actors/ships/weapon_set/weapons/projectile/bullet.gd
@@ -1,19 +1,12 @@
 class_name Bullet
-extends Node3D
-
+extends RayCast3D
 
 var linear_velocity := Vector3()
 
 
-
 func _physics_process(delta: float) -> void:
 	var new_position := global_transform.origin + linear_velocity * delta
-	var params := PhysicsRayQueryParameters3D.new()
-	params.from = global_transform.origin
-	params.to = new_position
-#	params.exclude = exclude
-	var result := get_world_3d().direct_space_state.intersect_ray(params)
-	global_transform.origin = new_position
-#	return result.position if result else Vector3()
-	if result:
+	target_position = to_local(new_position)
+	if is_colliding():
 		queue_free()
+	global_transform.origin = new_position
