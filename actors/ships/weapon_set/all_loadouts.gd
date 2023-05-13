13811794dab9e46b38604cd991b55baed57f202c
Martin Riesz <riesz.martin@gmail.com>
2023-05-13 14:07:20 +0200

    Add projectile weapon

--- actors/ships/weapon_set/all_loadouts.gd
+++ actors/ships/weapon_set/all_loadouts.gd
@@ -5,3 +5,8 @@ var twin_lasers := {
 	^"Left": Weapons.laser,
 	^"Right": Weapons.laser,
 }
+
+var laser_and_projectile := {
+	^"Left": Weapons.laser,
+	^"Right": Weapons.projectile,
+}
--- actors/ships/weapon_set/weapons/all_weapons.gd
+++ actors/ships/weapon_set/weapons/all_weapons.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var laser := preload("laser/laser.tscn")
+var projectile := preload("projectile/projectile.tscn")
