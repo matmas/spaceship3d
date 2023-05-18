8437874c9d38239379c9067217c7acfca9285a35
Martin Riesz <riesz.martin@gmail.com>
2023-05-18 02:52:49 +0200

    Calculate camera_position inside motion_particles shader

--- world/motion_particles/motion_particles.gd
+++ world/motion_particles/motion_particles.gd
@@ -2,15 +2,12 @@ extends GPUParticles3D
 
 @onready var camera := get_viewport().get_camera_3d()
 @onready var previous_camera_position := camera.global_position
-
-var camera_velocity := Vector3()
+@onready var particles_material := draw_pass_1.surface_get_material(0) as ShaderMaterial
 
 
 func _process(delta: float) -> void:
 	global_transform = camera.global_transform
-	var shader := draw_pass_1.surface_get_material(0) as ShaderMaterial
-	shader.set_shader_parameter(&"camera_position", camera.global_position)
-	shader.set_shader_parameter(&"camera_velocity", camera_velocity)
 
-	camera_velocity = (camera.global_position - previous_camera_position) / delta
+	var camera_velocity := (camera.global_position - previous_camera_position) / delta
+	particles_material.set_shader_parameter(&"camera_velocity", camera_velocity)
 	previous_camera_position = camera.global_position
