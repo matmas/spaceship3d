01828bef0b911ab80f1a922edf027d15a271adab
Martin Riesz <riesz.martin@gmail.com>
2023-05-22 17:45:14 +0200

    Add precise target lead aim assist indicator

--- actors/ships/weapon_set/weapons/weapon.gd
+++ actors/ships/weapon_set/weapons/weapon.gd
@@ -15,6 +15,7 @@ var is_fixed := false
 var try_firing := false
 var projectile_speed := INF
 var targetting_speed := 1.0
+var can_focus := true
 var target_position_override := Vector3()
 var is_ui_visible := false
 var selection: CollisionObject3D
@@ -48,21 +49,25 @@ func _process(delta: float) -> void:
 				selection_global_position,
 				(selection as RigidBody3D).linear_velocity,
 				Utils.interpolated_global_position(ship),
-				ship.linear_velocity,
 			)
 			target_lead.position = camera.unproject_position(collision_position)
 			target_lead.visible = not camera.is_position_behind(collision_position)
 
 
-func _calculate_projectile_and_target_collision_point(target_position: Vector3, target_velocity: Vector3, player_position: Vector3, player_velocity: Vector3, time: float = 0.01, recursion_depth: int = 20) -> Vector3:
-	var distance := target_position.distance_to(player_position)
-	var relative_velocity := target_velocity - player_velocity
-	var projectile_travel_time := 1.0 / projectile_speed
-	return target_position + relative_velocity * projectile_travel_time * distance
+func _calculate_projectile_and_target_collision_point(target_position: Vector3, target_velocity: Vector3, ship_position: Vector3, time: float = 0.01, max_recursion_depth: int = 20) -> Vector3:
+	var relative_velocity := target_velocity - ship.linear_velocity
+	var target_future_position := target_position + relative_velocity * time
+	var projectile_direction := ship_position.direction_to(target_future_position)
+	var projectile_velocity := projectile_direction * projectile_speed + ship.linear_velocity
+	var projectile_travel_distance := ship_position.distance_to(target_future_position)
+	var new_time := projectile_travel_distance / projectile_velocity.length()
+	if max_recursion_depth == 0 or is_equal_approx(new_time, time):
+		return target_future_position
+	return _calculate_projectile_and_target_collision_point(target_position, target_velocity, ship_position, new_time, max_recursion_depth - 1)
 
 
 func _physics_process(_delta: float) -> void:
-	if not is_fixed:
+	if not is_fixed and can_focus:
 		target_position_override = _mouse_cursor_to_collision_world_position()
 
 
