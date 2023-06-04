423eb0209b046d378d6e36d53105bc6ce2a07e18
Martin Riesz <riesz.martin@gmail.com>
2023-06-04 13:52:29 +0200

    Prevent error when Debug menu plugin is disabled

--- main.gd
+++ main.gd
@@ -2,4 +2,6 @@ extends Node
 
 
 func _ready() -> void:
-	DebugMenu.style = DebugMenu.Style.VISIBLE_DETAILED
+	var debug_menu := get_tree().root.get_node_or_null("DebugMenu")
+	if debug_menu:
+		debug_menu.style = debug_menu.Style.VISIBLE_DETAILED
