69e1c5e03afcc287b508d2ae7c880759ee6d9c38
Martin Riesz <riesz.martin@gmail.com>
2023-06-04 14:06:15 +0200

    Let Thruster._process sleep when power is zero

--- actors/ships/propulsion/thruster/thruster.gd
+++ actors/ships/propulsion/thruster/thruster.gd
@@ -17,7 +17,11 @@ func _process(delta: float) -> void:
 	flame.set_instance_shader_parameter(&"instance_gradient_multiplier", power)
 	flame.set_instance_shader_parameter(&"instance_gradient_shift", power)
 	exhaust.volume_db = linear_to_db(power * 0.1)
+	if is_zero_approx(power):
+		set_process(false)
 
 
 func set_power(value: float) -> void:
 	target_power = clampf(value, 0, 1)
+	if not is_zero_approx(target_power):
+		set_process(true)
