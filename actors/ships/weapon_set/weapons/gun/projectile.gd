689b5db68b019bd5cb5ea15e89a77bfcf140be72
Martin Riesz <riesz.martin@gmail.com>
2023-06-04 21:13:38 +0200

    Add projectile impacting sound

--- actors/ships/weapon_set/weapons/gun/projectile.gd
+++ actors/ships/weapon_set/weapons/gun/projectile.gd
@@ -6,9 +6,9 @@ extends Node3D
 @onready var smoke := $Smoke as GPUParticles3D
 @onready var camera := get_viewport().get_camera_3d()
 @onready var initial_global_position := global_position
-@onready var impact_shield := $ImpactShield as AudioStreamPlayer3D
+@onready var impacting := $Impacting as AudioStreamPlayer3D
+@onready var impacting_shield := $ImpactingShield as AudioStreamPlayer3D
 @onready var flying := $Flying as AudioStreamPlayer3D
-
 
 var linear_velocity := Vector3()
 var excluded_rids: Array[RID] = []
@@ -34,8 +34,11 @@ func _process(delta: float) -> void:
 			hittable.hit.emit(self, result.position)
 
 			if hittable is Shield:
-				impact_shield.pitch_scale = randf_range(0.8, 2.0)
-				impact_shield.play()
+				impacting_shield.pitch_scale = randf_range(0.8, 2.0)
+				impacting_shield.play()
+			else:
+				impacting.pitch_scale = randf_range(0.9, 1.1)
+				impacting.play()
 			flying.stop()
 
 			for p in [sparks, smoke]:
@@ -47,7 +50,7 @@ func _process(delta: float) -> void:
 					particles.look_at(result.position + result.normal)
 		set_process(false)
 		mesh_instance.visible = false
-		await get_tree().create_timer(maxf(maxf(sparks.lifetime, smoke.lifetime), impact_shield.stream.get_length())).timeout
+		await get_tree().create_timer(maxf(maxf(sparks.lifetime, smoke.lifetime), maxf(impacting.stream.get_length(), impacting_shield.stream.get_length()))).timeout
 		queue_free()
 	else:
 		global_position = params.to
