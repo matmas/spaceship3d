4ceebad2fbce7c677ae379e59a73d374ced2fb49
Martin Riesz <riesz.martin@gmail.com>
2023-06-22 11:24:51 +0200

    Do not show object ui in the corner of the screen

--- assets/shared/object_ui/object_ui.gd
+++ assets/shared/object_ui/object_ui.gd
@@ -19,17 +19,20 @@ func _process(_delta: float) -> void:
 	visible = not camera.is_position_behind(visual_instance.global_position)
 	if visible:
 		var rect := Utils.unproject_aabb_to_screen_space_rect(visual_instance, camera)
-		rect = Utils.make_square(rect, MIN_SIZE)
-		position = rect.position
-		size = rect.size
+		if rect:
+			rect = Utils.make_square(rect, MIN_SIZE)
+			position = rect.position
+			size = rect.size
 
-		var distance := "%.0f m" % camera.target.global_position.distance_to(visual_instance.global_position)
-		var velocity := ""
-		if body is RigidBody3D:
-			var body_ := body as RigidBody3D
-			velocity = "%.0f m/s" % body_.linear_velocity.length()
-		label.text = "%s\n%s\n%s" % [body.name, distance, velocity]
+			var distance := "%.0f m" % camera.target.global_position.distance_to(visual_instance.global_position)
+			var velocity := ""
+			if body is RigidBody3D:
+				var body_ := body as RigidBody3D
+				velocity = "%.0f m/s" % body_.linear_velocity.length()
+			label.text = "%s\n%s\n%s" % [body.name, distance, velocity]
 
-		if visual_instance.owner is Ship:
-			var ship := visual_instance.owner as Ship
-			modulate = Factions.get_color(ship.pilot.faction)
+			if visual_instance.owner is Ship:
+				var ship := visual_instance.owner as Ship
+				modulate = Factions.get_color(ship.pilot.faction)
+		else:
+			visible = false
