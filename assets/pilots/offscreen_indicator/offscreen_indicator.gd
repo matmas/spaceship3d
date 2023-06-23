6c7576ac6d8a9cbd73ec41413fd71f2b3682ea7e
Martin Riesz <riesz.martin@gmail.com>
2023-06-23 02:01:44 +0200

    Rename OffscreenMarker to OffscreenIndicator, change to Sprite2D

--- assets/pilots/offscreen_indicator/offscreen_indicator.gd
+++ assets/pilots/offscreen_indicator/offscreen_indicator.gd
@@ -1,7 +1,6 @@
-extends Node3D
+extends Sprite2D
 
-@onready var marker := $Marker as Sprite2D
-@onready var marker_material := marker.material as ShaderMaterial
+@onready var parent_node3d := get_parent() as Node3D
 @onready var camera := get_viewport().get_camera_3d()
 
 const PIXEL_OFFSET = 32.0
@@ -9,17 +8,17 @@ const MAX_CAMERA_DISTANCE = 1000.0
 
 
 func _process(_delta: float) -> void:
-	marker.visible = not camera.is_position_in_frustum(global_position)
-	if marker.visible:
-		var camera_distance := camera.global_position.distance_to(global_position)
+	visible = not camera.is_position_in_frustum(parent_node3d.global_position)
+	if visible:
+		var camera_distance := camera.global_position.distance_to(parent_node3d.global_position)
 		var alpha := clampf((MAX_CAMERA_DISTANCE - camera_distance) * 0.1, 0, 1)
-		marker_material.set_shader_parameter(&"alpha", alpha)
+		modulate.a = alpha
 		if alpha > 0:
 			# https://www.reddit.com/r/godot/comments/yovu0s/how_would_i_create_offscreen_indicators_in_a_3d/ivgg4i6/
 			var viewport_center := get_viewport().get_visible_rect().size * 0.5
-			var camera_to_position := camera.to_local(global_position)
+			var camera_to_position := camera.to_local(parent_node3d.global_position)
 			var center_to_edge := Vector2(camera_to_position.x, -camera_to_position.y)
 			var element := int(center_to_edge.abs().aspect() < viewport_center.aspect())
 			center_to_edge *= viewport_center[element] / maxf(absf(center_to_edge[element]), 0.0001)
-			marker.position = viewport_center + center_to_edge - center_to_edge.normalized() * PIXEL_OFFSET
-			marker.rotation = center_to_edge.angle() + TAU * 0.25
+			position = viewport_center + center_to_edge - center_to_edge.normalized() * PIXEL_OFFSET
+			rotation = center_to_edge.angle() + TAU * 0.25
--- assets/pilots/pilot.gd
+++ assets/pilots/pilot.gd
@@ -1,8 +1,7 @@
 class_name Pilot
 extends Area3D
 
-@onready var offscreen_marker := $PhysicsInterpolation/OffscreenMarker as Node3D
-@onready var offscreen_marker_material := $PhysicsInterpolation/OffscreenMarker/Marker.material as ShaderMaterial
+@onready var offscreen_indicator := $PhysicsInterpolation/OffscreenIndicator as Sprite2D
 @onready var ship := get_parent() as Ship
 
 var faction: Factions.Faction
@@ -13,8 +12,8 @@ func _ready() -> void:
 	ship.pilot = self
 	# pilots want to set ship weapon loadouts in their _ready so ship must be ready beforehand
 	await ship.ready
-	offscreen_marker.visible = false
-	offscreen_marker_material.set_shader_parameter(&"color", Factions.get_color(faction))
+	offscreen_indicator.visible = false
+	offscreen_indicator.modulate = Color(Factions.get_color(faction), offscreen_indicator.modulate.a)
 
 	ship.object_ui.set_color(Factions.get_color(faction))
 
@@ -23,11 +22,11 @@ func _on_detection_radar_area_entered(area: Area3D) -> void:
 	if area is Pilot and area != self:
 		var pilot := area as Pilot
 		detected_pilots[pilot] = true
-		pilot.offscreen_marker.visible = true
+		pilot.offscreen_indicator.visible = true
 
 
 func _on_tracking_radar_area_exited(area: Area3D) -> void:
 	if area is Pilot:
 		var pilot := area as Pilot
 		detected_pilots.erase(pilot)
-		pilot.offscreen_marker.visible = false
+		pilot.offscreen_indicator.visible = false
