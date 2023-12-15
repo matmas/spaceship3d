65f4b1dd43f58a5a8cf16f121ea9747806d83148
Martin Riesz <riesz.martin@gmail.com>
2023-12-15 02:23:32 +0100

    Update to Beehave 2.7.9

--- addons/beehave/nodes/beehave_tree.gd
+++ addons/beehave/nodes/beehave_tree.gd
@@ -1,7 +1,8 @@
-## Controls the flow of execution of the entire behavior tree.
 @tool
 @icon("../icons/tree.svg")
 class_name BeehaveTree extends Node
+
+## Controls the flow of execution of the entire behavior tree.
 
 enum {
 	SUCCESS,
@@ -9,15 +10,21 @@ enum {
 	RUNNING
 }
 
+enum ProcessThread {
+	IDLE,
+	PHYSICS
+}
+
 signal tree_enabled
 signal tree_disabled
 
-## Wether this behavior tree should be enabled or not.
+
+## Whether this behavior tree should be enabled or not.
 @export var enabled: bool = true:
 	set(value):
 		enabled = value
-		set_physics_process(enabled)
-
+		set_physics_process(enabled and process_thread == ProcessThread.PHYSICS)
+		set_process(enabled and process_thread == ProcessThread.IDLE)
 		if value:
 			tree_enabled.emit()
 		else:
@@ -27,8 +34,32 @@ signal tree_disabled
 	get:
 		return enabled
 
+
+## How often the tree should tick, in frames. The default value of 1 means 
+## tick() runs every frame.
+@export var tick_rate: int = 1
+
+
 ## An optional node path this behavior tree should apply to.
-@export_node_path var actor_node_path : NodePath
+@export_node_path var actor_node_path : NodePath:
+	set(anp):
+		actor_node_path = anp
+		if actor_node_path != null and str(actor_node_path) != "..":
+			actor = get_node(actor_node_path)
+		else:
+			actor = get_parent()
+		if Engine.is_editor_hint():
+			update_configuration_warnings()
+
+
+## Whether to run this tree in a physics or idle thread.
+@export var process_thread:ProcessThread = ProcessThread.PHYSICS:
+	set(value):
+		process_thread = value
+		set_physics_process(enabled and process_thread == ProcessThread.PHYSICS)
+		set_process(enabled and process_thread == ProcessThread.IDLE)
+		
+
 
 ## Custom blackboard node. An internal blackboard will be used
 ## if no blackboard is provided explicitly.
@@ -43,6 +74,11 @@ signal tree_disabled
 			_internal_blackboard = Blackboard.new()
 			add_child(_internal_blackboard, false, Node.INTERNAL_MODE_BACK)
 	get:
+		# in case blackboard is accessed before this node is,
+		# we need to ensure that the internal blackboard is used.
+		if not blackboard and not _internal_blackboard:
+			_internal_blackboard = Blackboard.new()
+			add_child(_internal_blackboard, false, Node.INTERNAL_MODE_BACK)
 		return blackboard if blackboard else _internal_blackboard
 
 ## When enabled, this tree is tracked individually
@@ -61,48 +97,68 @@ signal tree_disabled
 
 			BeehaveDebuggerMessages.unregister_tree(get_instance_id())
 
+
 var actor : Node
 var status : int = -1
+var last_tick : int = 0
 
 var _internal_blackboard: Blackboard
 var _process_time_metric_name : String
 var _process_time_metric_value : float = 0.0
 var _can_send_message: bool = false
 
+
 func _ready() -> void:
-	if Engine.is_editor_hint():
-		return
-
-	if self.get_child_count() > 0 and not self.get_child(0) is BeehaveNode:
-		push_warning("Beehave error: Root %s should have only one child of type BeehaveNode (NodePath: %s)" % [self.name, self.get_path()])
-		disable()
-		return
-
-	if not blackboard:
-		_internal_blackboard = Blackboard.new()
-		add_child(_internal_blackboard, false, Node.INTERNAL_MODE_BACK)
-
-	actor = get_parent()
+	if not process_thread:
+		process_thread = ProcessThread.PHYSICS
+	
 	if actor_node_path:
 		actor = get_node(actor_node_path)
+	else:
+		actor = get_parent()
 
+	if not blackboard:
+		# invoke setter to auto-initialise the blackboard.
+		self.blackboard = null
+	
 	# Get the name of the parent node name for metric
-	var parent_name = actor.name
-	_process_time_metric_name = "beehave [microseconds]/process_time_%s-%s" % [parent_name, get_instance_id()]
+	_process_time_metric_name = "beehave [microseconds]/process_time_%s-%s" % [actor.name, get_instance_id()]
 
+	set_physics_process(enabled and process_thread == ProcessThread.PHYSICS)
+	set_process(enabled and process_thread == ProcessThread.IDLE)
+	
 	# Register custom metric to the engine
-	if custom_monitor:
+	if custom_monitor and not Engine.is_editor_hint():
 		Performance.add_custom_monitor(_process_time_metric_name, _get_process_time_metric_value)
 		BeehaveGlobalMetrics.register_tree(self)
 
-	set_physics_process(enabled)
-	BeehaveGlobalDebugger.register_tree(self)
-	BeehaveDebuggerMessages.register_tree(_get_debugger_data(self))
+	if Engine.is_editor_hint():
+		update_configuration_warnings.call_deferred()
+	else:
+		BeehaveGlobalDebugger.register_tree(self)
+		BeehaveDebuggerMessages.register_tree(_get_debugger_data(self))
+
+	# Randomize at what frames tick() will happen to avoid stutters
+	last_tick = randi_range(0, tick_rate - 1)
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	_process_internally()
+	
+	
+func _process(_delta: float) -> void:
+	_process_internally()
+
+
+func _process_internally() -> void:
 	if Engine.is_editor_hint():
 		return
+
+	if last_tick < tick_rate - 1:
+		last_tick += 1 
+		return
+	
+	last_tick = 0
 
 	# Start timing for metric
 	var start_time = Time.get_ticks_usec()
@@ -123,6 +179,8 @@ func _physics_process(delta: float) -> void:
 
 
 func tick() -> int:
+	if actor == null or get_child_count() == 0:
+		return FAILURE
 	var child := self.get_child(0)
 	if status != RUNNING:
 		child.before_run(actor, blackboard)
@@ -142,6 +200,9 @@ func tick() -> int:
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings:PackedStringArray = []
+	
+	if actor == null:
+		warnings.append("Configure target node on tree")
 
 	if get_children().any(func(x): return not (x is BeehaveNode)):
 		warnings.append("All children of this node should inherit from BeehaveNode class.")
@@ -204,7 +265,7 @@ func _exit_tree() -> void:
 
 # Called by the engine to profile this tree
 func _get_process_time_metric_value() -> int:
-	return _process_time_metric_value
+	return int(_process_time_metric_value)
 
 
 func _get_debugger_data(node: Node) -> Dictionary:
