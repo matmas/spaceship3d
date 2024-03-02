961fa20a5e3929a900da1be8bd9ca61d3fafc4ae
Martin Riesz <riesz.martin@gmail.com>
2024-03-02 15:26:23 +0100

    Revert "Use Godot 4.1 obstacle avoidance"
    This reverts commit 8d654cfbf8a48bce7e8699723437b19365aa5ade.

--- assets/pilots/npc/npc.gd
+++ assets/pilots/npc/npc.gd
@@ -19,20 +19,20 @@ func _ready() -> void:
 	offscreen_indicator.modulate = Color(Factions.get_color(faction), offscreen_indicator.modulate.a)
 
 
-#func _physics_process(_delta: float) -> void:
-#	const collision_avoidance_importance = 1.5
-#	var collision_avoidance_thrust := Vector3()
-#	for node in proxymity_sensor.get_overlapping_bodies():
-#		collision_avoidance_thrust += _thrust_to_avoid(node.global_position, proxymity_distance) * collision_avoidance_importance
-#	ship.linear_acceleration_to_apply += collision_avoidance_thrust
+func _physics_process(_delta: float) -> void:
+	const collision_avoidance_importance = 1.5
+	var collision_avoidance_thrust := Vector3()
+	for node in proxymity_sensor.get_overlapping_bodies():
+		collision_avoidance_thrust += _thrust_to_avoid(node.global_position, proxymity_distance) * collision_avoidance_importance
+	ship.linear_acceleration_to_apply += collision_avoidance_thrust
 
 
-#func _thrust_to_avoid(target_position: Vector3, avoid_distance: float) -> Vector3:
-#	var target_relative_position := target_position - global_position
-#	var target_direction := target_relative_position.normalized()
-#	var target_distance := target_relative_position.length()
-#	var linear_acceleration := ship.max_linear_acceleration().z * (1.0 - minf(target_distance / avoid_distance, 1))
-#	return -target_direction * linear_acceleration
+func _thrust_to_avoid(target_position: Vector3, avoid_distance: float) -> Vector3:
+	var target_relative_position := target_position - global_position
+	var target_direction := target_relative_position.normalized()
+	var target_distance := target_relative_position.length()
+	var linear_acceleration := ship.max_linear_acceleration().z * (1.0 - minf(target_distance / avoid_distance, 1))
+	return -target_direction * linear_acceleration
 
 
 func _process_hit(_weapon: Weapon, source: Node3D, _impact_point: Vector3) -> void:
